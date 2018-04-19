437586eabf0e751a4f1a12b99a0410a0a20a139f
jankin <jankinliu@163.com>
2018-04-19 18:11:52 +0800

    接入thymeleaf

--- jankin-web/jankin-web-controller/src/main/java/com/jankin/web/controller/manage/ManageController.java
+++ jankin-web/jankin-web-controller/src/main/java/com/jankin/web/controller/manage/ManageController.java
@@ -3,6 +3,12 @@ package com.jankin.web.controller.manage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Hello word
@@ -20,5 +26,37 @@ public class ManageController {
     public String hello(){
         return "hello";
     }
+
+
+    @RequestMapping("thymeleaf")
+    @ResponseBody
+    public String thymeleafTest(){
+        //构造模板引擎
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        System.out.println(ManageController.class.getClassLoader().getParent());
+        //模板所在目录，相对于当前classloader的classpath。
+        resolver.setPrefix("templates/");
+        //模板文件后缀
+        resolver.setSuffix(".html");
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(resolver);
+
+        //构造上下文(Model)
+        Context context = new Context();
+        context.setVariable("name", "蔬菜列表");
+        context.setVariable("array", new String[]{"土豆", "番茄", "白菜", "芹菜"});
+
+        //渲染模板
+        FileWriter write = null;
+        try {
+            write = new FileWriter("example.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "fail";
+        }
+        templateEngine.process("example_template", context, write);
+        return "success";
+    }
+
 
 }
